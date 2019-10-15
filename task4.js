3f274ca5b2f06de83456bf180ed8d02fdcddb990
almasuddin <almas.bangladesh@gmail.com>
2019-10-15 20:47:44 +0600

    Express, jsonwebtoken are imported and port is defined

---
+++ task4.js
@@ -0,0 +1,12 @@
+//Import module 'express'
+const express = require('express');
+
+//Import module JSON Web Token
+const jwt = require('jsonwebtoken');
+
+//Create an object of express
+const app = express();
+
+//Define port as 5000
+const PORT = 5000;
+
