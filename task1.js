776af80adc136945ca18bdca983fd6d714b97fea
almasuddin <almas.bangladesh@gmail.com>
2019-10-15 18:41:03 +0600

    Actors list are added

---
+++ task1.js
@@ -0,0 +1,18 @@
+//Import module 'express'
+const express = require('express');
+
+//Create an object of express
+const app = express();
+
+//Create list of actors
+const actors = [
+    {name: 'Humayun Faridi',
+    birthday: 'May 29, 1952',
+    country: 'Bangladesh'},
+    {name: 'Zahid Hasan',
+    birthday: 'October 4, 1965',
+    country: 'Bangladesh'},
+    {name: 'Asaduzzaman Noor',
+    birthday: 'October 31, 1946',
+    country: 'Bangladesh'}
+];
