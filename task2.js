857414a4271416f1eb6565ef99ca237f851cae1d
almasuddin <almas.bangladesh@gmail.com>
2019-10-15 20:21:31 +0600

    Body-parser module imported. Routing for '/api/user/signup' is now returning token successfully

--- task2.js
+++ task2.js
@@ -2,7 +2,30 @@
 const express = require('express');
 
 //Import module JSON Web Token
-const jwt = require('jsonwebtoken')
+const jwt = require('jsonwebtoken');
+
+//Import module 'body-parser'
+var bodyParser = require('body-parser')
 
 //Create an object of express
 const app = express();
+
+// for parsing application/json
+app.use(bodyParser.json())
+
+//Define port as 5000
+const PORT = 5000;
+
+//POST request to "/api/user/signup" is made
+app.post('/api/user/signup', (req, res) =>{
+    jwt.sign({'user': req.body}, 'secretKey', (err, token) => {
+        res.json({
+            'token':token
+        })
+    })
+});
+
+//App is running on port (5000)
+app.listen(PORT, ()=>{
+    console.log("The app is running on port " + PORT);
+});
