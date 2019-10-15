04440fcbe41581df32cd03e58b6bc2ced9c7c4ec
almasuddin <almas.bangladesh@gmail.com>
2019-10-15 20:54:39 +0600

    List of movies are added as JSON format for task 4

--- task4.js
+++ task4.js
@@ -10,3 +10,40 @@ const app = express();
 //Define port as 5000
 const PORT = 5000;
 
+//Create list of movies
+var movies = [
+    {
+        title: 'Aynabaji',
+        year: '2016',
+        rating: '8.1',
+        actors: [
+            {
+            name: 'Chanchal Chowdhury',
+            birthday: '01 January, 1970',
+            country: 'Bangladesh'
+            },
+            {
+                name: 'Partha Barua',
+                birthday: '01 January, 1971',
+                country: 'Bangladesh'
+            }
+        ]
+    },
+    {
+        title: 'Debi',
+        year: '2018',
+        rating: '7.1',
+        actors: [
+            {
+            name: 'Chanchal Chowdhury',
+            birthday: '01 January, 1970',
+            country: 'Bangladesh'
+            },
+            {
+                name: 'Iresh Zaker',
+                birthday: '02 January, 1971',
+                country: 'Bangladesh'
+            }
+        ]
+    }
+];
