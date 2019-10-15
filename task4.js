6484dc7c2b607e6880af2aa36ba5f904b7c76da6
almasuddin <almas.bangladesh@gmail.com>
2019-10-15 22:18:17 +0600

    Task 4 is onging... but some problems are in authentication

--- task4.js
+++ task4.js
@@ -47,3 +47,48 @@ var movies = [
         ]
     }
 ];
+
+//Mock User
+const user = {
+    "username": "almas",
+    "password": "dhakalive"
+}
+
+//POST request to "/api/user/login" is made
+app.post('/api/user/login', (req, res) =>{
+    jwt.sign({'user': user}, 'secretKey', (err, token) => {
+        res.json({
+            'token':token
+        })
+    })
+});
+
+//GET request to "/api/movies" is made
+app.get('/api/movies', verifyToken, (req, res) => {  
+    //verify authentication code
+    jwt.verify(req.token, 'secretkey', (err, authData) => {
+        res.sendStatus(403);
+    });
+    
+  });
+
+// Verify Token
+function verifyToken(req, res, next) {
+    // Get auth header value
+    const bearerHeader = req.headers['authorization'];
+    // Check if bearer is undefined
+    if(typeof bearerHeader !== 'undefined') {
+      // Split at the space
+      const bearer = bearerHeader.split(' ');
+      // Get token from array
+      const bearerToken = bearer[0];
+      req.token = bearerToken;
+      next();
+    }
+  }
+
+//App is running on port (5000)
+app.listen(PORT, ()=>{
+    console.log("The app is running on port " + PORT);
+});
+
