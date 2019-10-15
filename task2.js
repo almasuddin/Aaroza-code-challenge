d53ac54a4974e17e1d3661c94f18aa367ec356d0
almasuddin <almas.bangladesh@gmail.com>
2019-10-15 19:14:47 +0600

    All required modules (express & jsonwebtoken are imported)

---
+++ task2.js
@@ -0,0 +1,8 @@
+//Import module 'express'
+const express = require('express');
+
+//Import module JSON Web Token
+const jwt = require('jsonwebtoken')
+
+//Create an object of express
+const app = express();
