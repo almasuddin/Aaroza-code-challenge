c4033b9fbfac33840c1ff58ed915acd5547430d2
almasuddin <almas.bangladesh@gmail.com>
2019-10-16 08:45:19 +0600

    login route accept username, password and successfully return token

--- routes/users.js
+++ routes/users.js
@@ -7,7 +7,7 @@ const bcrypt = require('bcrypt');
 //Import json web token module
 const jwt = require('jsonwebtoken');
 
-
+//Sign Up
 router.post('/signup', async (req, res)=>{
     // Check if user exist
     const userExist = await User.findOne({username: req.body.username});
@@ -33,5 +33,25 @@ router.post('/signup', async (req, res)=>{
         res.status(400).send(err);
     }
 });
+
+
+//Login
+router.post('/login', async (req, res) => {
+    //check if the username not exist
+    const user = await User.findOne({username: req.body.username});
+    if(!user){
+        return res.status(400).send('username not found');
+    }
+    //password is correct
+    const validPass = await bcrypt.compare(req.body.password, user.password)
+    if(!validPass){
+        return res.status(400).send("Invalid password");
+    }
+
+    //create and assign a token
+    const token = jwt.sign({username: user.username}, process.env.TOKEN_SECRET);
+    //show token
+    res.header('auth-token', token).send(token);
+})
 
 module.exports = router;
