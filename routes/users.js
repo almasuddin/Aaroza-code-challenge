e1daef72f29228b2c3fad2574c9e548586a4fda4
almasuddin <almas.bangladesh@gmail.com>
2019-10-16 07:50:37 +0600

    Function is written so that username is unique

--- routes/users.js
+++ routes/users.js
@@ -4,9 +4,17 @@ const router = require('express').Router();
 const User = require('../model/User');
 //Import module for hashing
 const bcrypt = require('bcrypt');
+//Import json web token module
+const jwt = require('jsonwebtoken');
 
 
 router.post('/signup', async (req, res)=>{
+    // Check if user exist
+    const userExist = await User.findOne({username: req.body.username});
+    if(userExist){
+        return res.status(400).send('user already exists');
+    }
+
     //Hash password
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(req.body.password, salt);
