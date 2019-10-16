6d5b4a8596828d6135b5144b8282dd7caf238eda
almasuddin <almas.bangladesh@gmail.com>
2019-10-16 07:44:30 +0600

    Function is added to insert data into database. And password is encrypted

---
+++ routes/users.js
@@ -0,0 +1,24 @@
+//Import Router from Express
+const router = require('express').Router();
+//Import database schema
+const User = require('../model/User');
+
+router.post('/signup', async (req, res)=>{
+    //Hash password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+    // Create a new user
+    const user = new User({
+        username: req.body.name,
+        password: hashedPassword
+    });
+    try {
+        const savedUser = await user.save();
+        res.send(savedUser);
+    } catch (error) {
+        res.status(400).send(err);
+    }
+});
+
+module.exports = router;
